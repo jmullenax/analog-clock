c7c1f59499f8dffc58da16c4942d1910a40ca5dc
jmullenax <jmullenax0001@email.vccs.edu>
2015-01-11 23:23:58 -0500

    seperated the hands from the face, the face is drawn once, the hands continuously.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-  //var $clock = $('canvas#clock');
-  var clock = document.getElementById('clock');
-  if (clock.getContext) {
-    var ctx = clock.getContext("2d");
-
+  
+  var clockHandsLayerElement = document.getElementById('clockHandsLayer');
+  var clockFaceLayerElement = document.getElementById('clockFaceLayer');
+  if (clockHandsLayerElement.getContext && clockFaceLayerElement.getContext) {
+    var clockHandsLayer = clockHandsLayerElement.getContext("2d");
+    var clockFaceLayer = clockFaceLayerElement.getContext("2d");
 
     function Hand(center, clockSize, outset) {
       this.step = null;
@@ -32,7 +33,7 @@ $(document).ready(function(){
       return ret;
     }
 
-    Hand.prototype.draw = function(config) {
+    Hand.prototype.draw = function(context, config) {
 
         for(option in config) {
           this[option] = config[option];
@@ -46,17 +47,17 @@ $(document).ready(function(){
           }
         }
 
-        ctx.strokeStyle = this.color;
-        ctx.lineWidth = this.thickness;
-        ctx.beginPath();          
+        context.strokeStyle = this.color;
+        context.lineWidth = this.thickness;
+        context.beginPath();          
         var new_x = this.getX(this.center.x, this.outset);
         var new_x_2 =  this.getX(this.center.x, this.end);
         var new_y = this.getY(this.center.y, this.outset);
         var new_y_2 =  this.getY(this.center.y, this.end);
 
-        ctx.moveTo( new_x, new_y );
-        ctx.lineTo( new_x_2, new_y_2 );
-        ctx.stroke();
+        context.moveTo( new_x, new_y );
+        context.lineTo( new_x_2, new_y_2 );
+        context.stroke();
     };
 
 
@@ -71,28 +72,31 @@ $(document).ready(function(){
       }
       this.log = false;
       this.center = {x:centerX, y:centerY};
-    }
-
-    
+    }  
 
     Clock.prototype.init = function() {
       // body...
     };
       
-    Clock.prototype.draw = function() {
-      this.drawCenterCircle(this.size/2 - this.size/50);        
+    Clock.prototype.draw = function(context) {
+      this.drawCenterCircle(context, this.size/2 - this.size/50);        
       
-      this.drawTicks();
-      this.drawCenterCircle(this.size/3);
-      this.drawCenterCircle(this.size/30, 'red');
-      this.drawCenterCircle(this.size/50);
+      this.drawTicks(context);
+      this.drawCenterCircle(context, this.size/3);
+      this.drawCenterCircle(context, this.size/30, 'red');
+      this.drawCenterCircle(context, this.size/50);
     };
 
     Clock.prototype.run = function() {
         var that = this;
+        var hands = clockHandsLayer;
+        var face = clockFaceLayer;
+
+        that.draw(face);
+
         window.setInterval(function(){
-              ctx.clearRect(that.center.x - that.size/2, that.center.y - that.size/2, that.size, that.size);
-              that.draw();
+              hands.clearRect(that.center.x - that.size/2, that.center.y - that.size/2, that.size, that.size);
+              
               var time = new Date();
               var seconds = (time.getMilliseconds()/1000)+time.getSeconds();
               var minutes = time.getMinutes()+(seconds/60);
@@ -101,18 +105,17 @@ $(document).ready(function(){
               var minutesHand = new Hand(that.center, that.size, -25);
               var hoursHand = new Hand(that.center, that.size);
               
-              minutesHand.draw({step: minutes, end: that.size*.45, thickness: that.size/50});
-              hoursHand.draw({scale: 12,step: hours, end: that.size*.2, thickness: that.size/50});
-              secondsHand.draw({step: seconds, outset: that.size/-20, end: that.size/3 + that.size/30, thickness: that.size/150, color: 'red'});
+              minutesHand.draw(hands, {step: minutes, end: that.size*.45, thickness: that.size/50});
+              hoursHand.draw(hands, {scale: 12,step: hours, end: that.size*.2, thickness: that.size/50});
+              secondsHand.draw(hands, {step: seconds, outset: that.size/-20, end: that.size/3 + that.size/30, thickness: that.size/150, color: 'red'});
               
               if(that.log) {
                 console.log('H:M:S '+hours+':'+minutes+':'+seconds);
-              }
-                
+              }                
           }, 100);
     };
 
-    Clock.prototype.drawTicks = function() {
+    Clock.prototype.drawTicks = function(context) {
       var tickLength = (this.size / 10) - (this.size/100);
       var tickOutset = this.size / 3;
       var tickEnd = tickLength + tickOutset;
@@ -123,29 +126,29 @@ $(document).ready(function(){
       var minuteTicks = new Hand(this.center, this.size);
 
       for(var q = 0; q < num_ticks; q++) {
-        minuteTicks.draw({step: q, scale: num_ticks, outset: tickOutset, end: tickEnd, width: width, color: color});
+        minuteTicks.draw(context, {step: q, scale: num_ticks, outset: tickOutset, end: tickEnd, width: width, color: color});
       }
 
       for(var q = 0; q < 60; q++) {
         if(q%5 == 0) continue;
-        hourTicks.draw({step: q, outset: tickOutset, end: tickEnd, width: this.size/300, color: '#ccc'});
+        hourTicks.draw(context, {step: q, outset: tickOutset, end: tickEnd, width: this.size/300, color: '#ccc'});
       }
     };
 
-    Clock.prototype.drawCenterCircle = function(size, color) {
-      this.drawCircle(this.center.x, this.center.y, size, color);
+    Clock.prototype.drawCenterCircle = function(context, size, color) {
+      this.drawCircle(context, this.center.x, this.center.y, size, color);
     }
 
-    Clock.prototype.drawCircle = function(x, y, diameter, color) {
+    Clock.prototype.drawCircle = function(context, x, y, diameter, color) {
        if(color === undefined) {
           color = 'steelblue';
         }
 
-        ctx.lineWidth = this.size/150;
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.arc(x,y,diameter,0,Math.PI*2,true);
-        ctx.stroke();
+        context.lineWidth = this.size/150;
+        context.beginPath();
+        context.strokeStyle = color;
+        context.arc(x,y,diameter,0,Math.PI*2,true);
+        context.stroke();
     };
 
     Clock.prototype.setLog = function(set){
